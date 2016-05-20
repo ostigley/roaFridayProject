4a8dc4375d83970cedea0cfec1b0d92c29c60f8b
Uili Fecteau & Jenny Lee <pair+Uili-Simteau.jennyArtorias@devbootcamp.com>
2016-05-20 13:47:14 +1200

    added more seeding tweets

--- seeds/addTweets.js
+++ seeds/addTweets.js
@@ -19,17 +19,17 @@ exports.seed = function(knex, Promise) {
     knex('tweets').insert({id: 12, tweet: 'OMG ROa', time_stamp: '', user_id: '2'}),
     knex('tweets').insert({id: 13, tweet: 'FunfunRoa!', time_stamp: '', user_id: '3'}),
     knex('tweets').insert({id: 14, tweet: 'objects are over rated', time_stamp: '', user_id: '4'}),
-    knex('tweets').insert({id: 15, tweet: 'ES7, really?', time_stamp: '', user_id: '5'})
-    knex('tweets').insert({id: 16, tweet: 'wisdom begins with humility', time_stamp: '', user_id: '7'})
-    knex('tweets').insert({id: 17, tweet: 'how many different mistakes to go until i become an expert?', time_stamp: '', user_id: '6'})
-    knex('tweets').insert({id: 18, tweet: 'just one breath away from calm', time_stamp: '', user_id: '8'})
-    knex('tweets').insert({id: 19, tweet: 'unix', time_stamp: '', user_id: '9'})
-    knex('tweets').insert({id: 20, tweet: 'rowValue', time_stamp: '', user_id: '4'}),
-    knex('tweets').insert({id: 21, tweet: 'rowValue', time_stamp: '', user_id: '5'})
-    knex('tweets').insert({id: 22, tweet: 'rowValue', time_stamp: '', user_id: '7'})
-    knex('tweets').insert({id: 23, tweet: 'rowValue', time_stamp: '', user_id: '6'})
-    knex('tweets').insert({id: 24, tweet: 'rowValue', time_stamp: '', user_id: '8'})
-    knex('tweets').insert({id: 25, tweet: 'rowValue', time_stamp: '', user_id: '9'})
+    knex('tweets').insert({id: 15, tweet: 'ES7, really?', time_stamp: '', user_id: '5'}),
+    knex('tweets').insert({id: 16, tweet: 'wisdom begins with humility', time_stamp: '', user_id: '7'}),
+    knex('tweets').insert({id: 17, tweet: 'how many different mistakes to go until i become an expert?', time_stamp: '', user_id: '6'}),
+    knex('tweets').insert({id: 18, tweet: 'just one breath away from calm', time_stamp: '', user_id: '8'}),
+    knex('tweets').insert({id: 19, tweet: 'unix time sux!', time_stamp: '', user_id: '9'}),
+    knex('tweets').insert({id: 20, tweet: 'doooooong, circle time :)', time_stamp: '', user_id: '4'}),
+    knex('tweets').insert({id: 21, tweet: 'Wisdom 1: I dont know', time_stamp: '', user_id: '5'}),
+    knex('tweets').insert({id: 22, tweet: 'Wisdom 2: I was wrong', time_stamp: '', user_id: '7'}),
+    knex('tweets').insert({id: 23, tweet: 'Wisdom 3: I am sorry', time_stamp: '', user_id: '6'}),
+    knex('tweets').insert({id: 24, tweet: 'Wisdom 4: I need help', time_stamp: '', user_id: '8'}),
+    knex('tweets').insert({id: 25, tweet: 'Nothing more humorous than an empty object {}.', time_stamp: '', user_id: '9'})
 
 
   );
