b822e32c187a606597dc242ae53a3d411515205b
Hape Cook-Porter & Oliver Stigley <pair+hapekituarangi.ostigley@devbootcamp.com>
2016-05-20 13:35:57 +1200

    jade homepage

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Chat Express' });
 });
-
+router.get('/tweets', function(req, res, next) {
+  res.render('index', { title: 'Chat Express' });
+});
 
 
 module.exports = router;
