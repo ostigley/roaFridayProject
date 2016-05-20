7958257e3b0c0d99cfa7c8e5e9258801d3eb339d
surya-surendranath <email": null>
2016-05-20 13:32:09 +1200

    router basics

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var model = require('../model/model')
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Chat Express' });
 });
 
+/* GET add new user page */
+
+router.get('/new-user', function(req, res, next) {
+     res.render('new-user', { title: 'Chat Express' });
+    });
+
+/* POST new user page */
+
+router.post('/new-user', function(req, res, next) {
+     res.redirect('/');
+    });
+
+/* GET existing user */
+
+router.get('/user', function(req, res, next) {
+     res.render('user', { title: 'Chat Express' });
+    });
+
+/* POST existing user */
+
+router.post('/user', function(req, res, next) {
+     res.redirect('/');
+    });
 
 
 module.exports = router;
